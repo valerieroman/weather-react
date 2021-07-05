2d5fb323023c8c81320f33cd803ed950eaf01af5
valerieroman <81587687+valerieroman@users.noreply.github.com>
2021-07-05 09:45:40 -0400

    Added rel="noreferrer" to Footer link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
     return (
         <p className="footer-cont">
-          <a href= "https://github.com/valerieroman/weather-react" target="_blank">
+          <a href= "https://github.com/valerieroman/weather-react" target="_blank" rel="noreferrer">
           Open source code by Valerie Roman De Jesus
           </a>
           </p>
